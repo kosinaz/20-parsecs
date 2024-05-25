09ee7914640db2ffd9c762377988627e926816d8
Kosina Zoltán <kosinaz@gmail.com>
2024-05-25 10:05:07 +0200

    update failed buttons

--- 20 Parsecs/scenes/valign.gd
+++ 20 Parsecs/scenes/valign.gd
@@ -1,5 +1,8 @@
 tool class_name RichTextVAlign extends RichTextEffect
 
+# Custom Effects: Array[1]: RichTextVAlign
+# [img=16]icon.png[/img][valign px=8]Hello World[/valign]
+
 var bbcode := "valign"
 
 func _process_custom_fx(char_fx:CharFXTransform) -> bool:
